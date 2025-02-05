c10d346348f0672e9cdbc96ef1b06f4f888abad3
Yung <yungleeviquez26@gmail.com>
2025-02-04 19:04:36 -0600

    Ejercicio_01

--- Week_04/Ejercicios.py
+++ Week_04/Ejercicios.py
@@ -1,2 +1,23 @@
 # Cree un programa que le pida al usuario su nombre, apellido, y edad, y muestre si es un bebé, niño, preadolescente, adolescente, adulto joven, adulto, o adulto mayor.
 
+
+nombre = input("¿Cual es su nombre?: ")
+apellido = input("¿Cual es su apellido:?: ")
+edad = int(input("¿Cual es su edad?: "))
+
+if edad <= 2:
+    categoria = "bebe"
+elif edad <= 12:
+    categoria = "niño"
+elif edad <= 14:
+    categoria = "preadolecente"
+elif edad <= 19:
+    categoria = "adolescente"
+elif edad <= 35:
+    categoria = "adulto joven"
+elif edad <= 59:
+    categoria = "adulto"
+else:
+    categoria = "adulto mayor"
+
+print (f"{nombre}{apellido}, con {edad} años , eres un {categoria}")
